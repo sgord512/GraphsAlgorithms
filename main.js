8b43a090c7a922b12be7df7510b7bea7b99d9c2a
Spencer Gordon <sgord512@gmail.com>
2012-02-28 21:34:22 -0500

    separated out the haskell, and everything modularized for Kruskal

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-require(['d3/d3', 'graphs', 'algorithms1'], function() { console.log("Everything loaded"); })
+require(['d3/d3', 'graphs', 'algorithms'], function() { console.log("Everything loaded"); })
